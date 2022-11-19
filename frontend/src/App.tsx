a94cef5d09dd85e3f7d7867ea2d8de3b00ee1c37
joao garcia <jiboiadamor@gmail.com>
2022-11-19 11:48:11 +0000

    the react crud app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Posts from './features/posts/Posts'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div className="App container">
+      <Posts />
     </div>
   );
 }
